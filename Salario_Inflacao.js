f74163e393e23ec55769561e07429d040127c6f5
Nitchinho <nitchinho007@gmail.com>
2023-08-14 12:08:09 -0300

    Coleção de Dados adicionado

---
+++ Salario_Inflacao.js
@@ -0,0 +1,29 @@
+const lista_salarios = [
+    {ano: 2010, salario: 510.00},
+    {ano: 2011, salario: 545.00},
+    {ano: 2012, salario: 622.00},
+    {ano: 2013, salario: 678.00},
+    {ano: 2014, salario: 724.00},
+    {ano: 2015, salario: 788.00},
+    {ano: 2016, salario: 880.00},
+    {ano: 2017, salario: 937.00},
+    {ano: 2018, salario: 954.00},
+    {ano: 2019, salario: 998.00},
+    {ano: 2020, salario: 1045.00},
+]
+
+const lista_inflacao = [
+    {ano: 2010, inflacao: 5.91},
+    {ano: 2011, inflacao: 6.50},
+    {ano: 2012, inflacao: 5.84},
+    {ano: 2013, inflacao: 5.91},
+    {ano: 2014, inflacao: 6.41},
+    {ano: 2015, inflacao: 10.67},
+    {ano: 2016, inflacao: 6.29},
+    {ano: 2017, inflacao: 2.95},
+    {ano: 2018, inflacao: 3.75},
+    {ano: 2019, inflacao: 4.31},
+    {ano: 2020, inflacao: 4.52},
+
+]
+
